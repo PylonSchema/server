4e14d209e338e6a3a5369e079c6ca27987d18922
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-02-01 18:10:07 +0900

    feature connect db with config file

--- server/server.go
+++ server/server.go
@@ -1,10 +1,22 @@
 package server
 
 import (
+	"github.com/BurntSushi/toml"
 	githubAuth "github.com/devhoodit/sse-chat/auth/github"
-	// "github.com/devhoodit/sse-chat/database"
+	"github.com/devhoodit/sse-chat/database"
 	"github.com/gin-gonic/gin"
 )
+
+type conf struct {
+	Database *databaseInfo
+}
+
+type databaseInfo struct {
+	Username string `toml:"username"`
+	Password string `toml:"password"`
+	Address  string `toml:"address"`
+	Port     string `toml:"port"`
+}
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
@@ -12,11 +24,19 @@ func SetupRouter() *gin.Engine {
 	// MiddleWare setting, server/middleware.go
 	setMiddleWare(r)
 
-	// d, err := database.Connect()
+	var conf conf
+	_, err := toml.DecodeFile("conf.toml", &conf)
+	if err != nil {
+		panic(err)
+	}
 
-	// if err != nil {
-	// 	panic(err)
-	// }
+	d, err := database.Connect(conf.Database.Username, conf.Database.Password, conf.Database.Address, conf.Database.Port)
+
+	if err != nil {
+		panic(err)
+	}
+
+	githubRouter := githubAuth.Github{DB: d}
 
 	r.GET("/", func(c *gin.Context) {
 	})
@@ -29,8 +49,8 @@ func SetupRouter() *gin.Engine {
 		}
 		github := auth.Group("/github")
 		{
-			github.GET("/login", githubAuth.RenderAuthView)
-			github.GET("/callback", githubAuth.Authenticate)
+			github.GET("/login", githubRouter.RenderAuthView)
+			github.GET("/callback", githubRouter.Authenticate)
 		}
 	}
 
