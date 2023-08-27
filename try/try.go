09bb046ded7313aecdfd9cf848d8cef27230a33c
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-08-27 15:22:30 +0900

    feat: try, fail state

--- try/try.go
+++ try/try.go
@@ -2,16 +2,21 @@ package try
 
 type Try interface {
 	Run() error
+	Fail() error
 }
 
-func TryN(t *Try, n int) error {
+func TryN(t Try, n int) error {
 	var err error
 	for i := 0; i < n; i++ {
-		err = (*t).Run()
+		err = t.Run()
 		if err != nil {
 			continue
 		}
 		return nil
+	}
+	err = t.Fail()
+	if err != nil {
+		// Fatal error
 	}
 	return err
 }
