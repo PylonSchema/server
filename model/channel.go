5475e0c532c236653fe675db4ea0258d1c8b1ad6
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-08-31 16:35:01 +0900

    fix: user, channel model

--- model/channel.go
+++ model/channel.go
@@ -11,12 +11,19 @@ type Channel struct {
 	Name      string
 	UUID      uuid.UUID
 	Owner     uuid.UUID
-	CreatedAt time.Time       `grom:"autoCreateTime"`
-	UpdatedAt time.Time       `gorm:"autoUpdateTime"`
-	Members   []ChannelMember `gorm:"foreignKey:ChannelId;References:Id;constraint:OnDelete:CASCADE"`
+	CreatedAt time.Time        `gorm:"autoCreateTime"`
+	UpdatedAt time.Time        `gorm:"autoUpdateTime"`
+	Members   []*ChannelMember `gorm:"foreignKey:ChannelId;References:Id;constraint:OnDelete:CASCADE"`
 }
 
 type ChannelMember struct {
 	ChannelId uint `gorm:"index"`
 	UUID      uuid.UUID
+}
+
+type InvitationChannel struct {
+	ChannelUUID     uuid.UUID `gorm:"index"`
+	InvitiationUUID string    `gorm:"index;type:varchar(125)"` // random invitation uuid + random string (15)
+	CreatedAt       time.Time `gorm:"autoCreateTime"`
+	ExpireAt        time.Time
 }
